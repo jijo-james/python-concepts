a4aa25f3dada8dcfd83ea9bec1bba48bf2234b95
Jijo James <jj.jamesjijo@gmail.com>
2023-01-17 00:08:37 +0530

    progress...

--- generator.py
+++ generator.py
@@ -38,18 +38,18 @@ for num in my_num:
 ######
 # calulating performance in generators
 
-import mem_profile
+import memory_profiler
 import random
 import time
 
 names = ['John', 'Corey', 'Adam', 'Steve', 'Rick', 'Thomas']
 majors = ['Math', 'Engineering', 'CompSci', 'Arts', 'Business']
 
-print ('Memory (Before): {}Mb'.format(mem_profile.memory_usage_psutil()))
+print ('Memory (Before): {}Mb'.format(memory_profiler.memory_usage()))
 
 def people_list(num_people):
     result = []
-    for i in xrange(num_people):
+    for i in range(num_people):
         person = {
                     'id': i,
                     'name': random.choice(names),
@@ -59,7 +59,7 @@ def people_list(num_people):
     return result
 
 def people_generator(num_people):
-    for i in xrange(num_people):
+    for i in range(num_people):
         person = {
                     'id': i,
                     'name': random.choice(names),
@@ -67,13 +67,13 @@ def people_generator(num_people):
                 }
         yield person
 
-# t1 = time.clock()
-# people = people_list(1000000)
-# t2 = time.clock()
+#t1 = time.time()
+#people = people_list(1000000)
+#t2 = time.time()
 
-t1 = time.clock()
+t1 = time.time()
 people = people_generator(1000000)
-t2 = time.clock()
+t2 = time.time()
 
-print ('Memory (After) : {}Mb'.format(mem_profile.memory_usage_psutil()))
+print ('Memory (After) : {}Mb'.format(memory_profiler.memory_usage()))
 print ('Took {} Seconds'.format(t2-t1))
