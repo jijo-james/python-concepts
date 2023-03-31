d84b170b9b82136a68615fcd89cae948700c86ea
Jijo James <jj.jamesjijo@gmail.com>
2023-03-31 23:13:32 +0530

    /practice 2 probs

---
+++ practice/squareroot_prime.py
@@ -0,0 +1,25 @@
+original_list = [1,4,9,16,25,36,49,56]
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2,int((num**.5)+1)):
+        if num % i == 0:
+            print(num)
+            return False
+    return True
+
+# # simple method
+# new_list = []
+
+# for num in original_list:
+#     sqrt_num = num**.5
+#     if is_prime(sqrt_num):
+#         new_list.append(num)
+
+
+# using list comprehension
+
+new_list = [num for num in original_list if is_prime(num**.5)]
+
+print(new_list)
---
+++ practice/test.py
@@ -0,0 +1,50 @@
+# p = W/t
+# S = D/t
+ 
+# Distance is always positive
+# S>1 then calculate power
+# S<0 then throw exception and cal power
+ 
+# format
+# 'company' : [distance, time, work]
+ 
+car = {         
+'BMW' : [25, 40, 200],
+'MB' : [50, 20, 400],
+'AUDI' : [40, 30.5, 320],
+'LAM' : [-40, 30.5, 320]
+}
+
+# print(type(car.values()))
+
+
+class Cars():
+
+    def __init__(self):
+        return
+    
+    def calculations(self, car):
+        for key, value in car.items():
+            try:
+                if value[0] > 0:
+                    speed = value[0]/value[1]
+                    print(speed)
+                
+                    if speed > 1:
+                        power = value[2]/value[1]
+                    try:
+                        if 0 < speed <1:
+                            pass
+                    except:
+                        print("Speed is {}, which is < 0".format(speed))
+                        power = value[2]/value[1]
+            except:
+                print("Distance is , which is a negative value")
+            
+            print( speed, power)
+
+
+c = Cars()
+
+c.calculations(car)
+
