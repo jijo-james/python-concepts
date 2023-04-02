3d2040685b58eab56e5657a8bb3dfe9c0ea9229c
Jijo James <jj.jamesjijo@gmail.com>
2023-04-02 23:53:31 +0530

    practice/ anagram sol3

--- practice/leetcode_242_anagram.py
+++ practice/leetcode_242_anagram.py
@@ -1,15 +1,15 @@
 class Solution:
-    
+
     def is_anagram(self, s: str, t: str):
         if len(s) != len(t):
             return False
-        
+
         count_s, count_t = {},{}
 
         for i in range(len(s)):
             count_s[s[i]] = 1 + count_s.get(s[i], 0)
             count_t[t[i]] = 1 + count_t.get(t[i], 0)
-        
+
         for c in count_s[c]:
             if count_s[c] != count_t.get(c, 0):
                 return False
@@ -25,3 +25,13 @@ print(s.is_anagram('jijo', 'james'))
 
 # print (Counter('jijo') == Counter('jjio'))
 
+# solution 3
+
+# class Solution:
+
+#     def is_anagram(self, s: str, t: str):
+#         return sorted(s) == sorted(t)
+
+# s = Solution()
+
+# print(s.is_anagram('jijo', 'jjio'))
