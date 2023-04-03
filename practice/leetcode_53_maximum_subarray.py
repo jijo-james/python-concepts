b28b1a2072f0d19bd7610d28afe3273be70a1541
Jijo James <jj.jamesjijo@gmail.com>
2023-04-04 00:01:39 +0530

    practice/ max subarray execution

--- practice/leetcode_53_maximum_subarray.py
+++ practice/leetcode_53_maximum_subarray.py
@@ -9,3 +9,8 @@ class Solution:
                 current_sum = 0
             current_sum += n
             max_subarray = max(max_subarray, current_sum)
+        return max_subarray
+
+s = Solution()
+
+print(s.max_subarray([1, 3, -5, 2, 7, -6, 5])) 
