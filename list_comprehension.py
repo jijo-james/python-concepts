1e2c18192b0de626f4020f11a69128ad3f95bd9b
Jijo James <jj.jamesjijo@gmail.com>
2023-04-07 14:28:53 +0530

    list comprehension advanced

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,7 +1,9 @@
 nums = [1,2,3,4,5,6,7,8,9,10]
 
-my_list = map(lambda n : n*n, nums)
+# my_list = map(lambda n : n*n, nums)
 
 # my_list = [ n*n for n in nums]
 
-print (next( my_list))
+my_list = [n for n in nums if n%2 ==0]
+
+print (my_list)
