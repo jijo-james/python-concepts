c886b4bc2639c500c00cbde0ec3e1ef6039f7134
Jijo James <jj.jamesjijo@gmail.com>
2023-04-07 14:33:19 +0530

    list comprehension filter

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,9 +1,14 @@
-nums = [1,2,3,4,5,6,7,8,9,10]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-# my_list = map(lambda n : n*n, nums)
+# my_list = map(lambda n : n*n, numbers)
+# it creates a map object, either iterate through it
+# or get each next element with 'next' fn
+# or convert it to a list using 'list()' function
 
-# my_list = [ n*n for n in nums]
+# my_list = [ n*n for n in numbers]
 
-my_list = [n for n in nums if n%2 ==0]
+# my_list = [n for n in numbers if n%2 ==0]
 
-print (my_list)
+my_list = list(filter(lambda n: n%2 == 0, numbers))
+
+print(my_list)
