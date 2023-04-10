3fcf90585095180ea6496f32c96f8999e54a0bbf
Jijo James <jj.jamesjijo@gmail.com>
2023-04-10 23:55:21 +0530

    practice/ quadratic eqn real soln

---
+++ practice/quadratic_equation.py
@@ -0,0 +1,22 @@
+# The standard form of a quadratic equation is:
+
+# ax2 + bx + c = 0, where
+# a, b and c are real numbers and
+# a ≠ 0
+# The solutions of this quadratic equation is given by:
+
+# (-b ± (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
+
+# sol 1
+# for real solution
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
+
+d = (b**2) - (4*a*c)
+
+solution_1 = (-b + d**.5)/(2*a)
+solution_2 = (-b - d**.5)/(2*a)
+
+print("Solutions for quadratic equation are {} and {}". format(solution_1, solution_2))
