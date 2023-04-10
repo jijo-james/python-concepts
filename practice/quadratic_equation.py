a6b5c6bd61ea70edba1c712da5d7ad85dead0197
Jijo James <jj.jamesjijo@gmail.com>
2023-04-10 23:58:23 +0530

    practice/ quadratic eqn imaginary soln

--- practice/quadratic_equation.py
+++ practice/quadratic_equation.py
@@ -10,13 +10,31 @@
 # sol 1
 # for real solution
 
+# a = int(input("Enter a: "))
+# b = int(input("Enter b: "))
+# c = int(input("Enter c: "))
+
+# d = (b**2) - (4*a*c)
+
+# solution_1 = (-b + d**.5)/(2*a)
+# solution_2 = (-b - d**.5)/(2*a)
+
+# print("Solutions for quadratic equation are {} and {}". format(solution_1, solution_2))
+
+
+
+# sol 2
+# for imaginary solution
+
+from cmath import sqrt
+
 a = int(input("Enter a: "))
 b = int(input("Enter b: "))
 c = int(input("Enter c: "))
 
 d = (b**2) - (4*a*c)
 
-solution_1 = (-b + d**.5)/(2*a)
-solution_2 = (-b - d**.5)/(2*a)
+solution_1 = (-b + sqrt(d))/(2*a)
+solution_2 = (-b - sqrt(d))/(2*a)
 
 print("Solutions for quadratic equation are {} and {}". format(solution_1, solution_2))
