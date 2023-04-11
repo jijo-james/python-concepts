e0629c7d50559d84995e9d467511818b75b143cc
Jijo James <jj.jamesjijo@gmail.com>
2023-04-12 00:34:04 +0530

    practice/ factorial-if-else

--- practice/factorial.py
+++ practice/factorial.py
@@ -2,16 +2,35 @@
 # using recursion
 
 
-def factorial(x):
+# def factorial(x):
 
-    if x == 1:
-        return 1
-    else:
-        # recursive call to the function
-        return x * factorial(x - 1)
+#     if x == 1:
+#         return 1
+#     else:
+#         # recursive call to the function
+#         return x * factorial(x - 1)
 
+
+# num = int(input("Enter a number: "))
+
+# result = factorial(num)
+# print("The factorial of", num, "is", result)
+
+
+
+
+# using if else
 
 num = int(input("Enter a number: "))
 
-result = factorial(num)
-print("The factorial of", num, "is", result)
+factorial = 1
+
+# check if the number is negative, positive or zero
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
