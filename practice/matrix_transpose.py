41c555f715322d5ace137f9715ab8b3f856e040a
Jijo James <jj.jamesjijo@gmail.com>
2023-04-24 23:41:22 +0530

    practice/ matrix transpose using list comprehension

--- practice/matrix_transpose.py
+++ practice/matrix_transpose.py
@@ -1,17 +1,29 @@
-# method 1
-# Program to transpose a matrix using a nested loop
+# # method 1
+# # Program to transpose a matrix using a nested loop
+
+# X = [[12,7],
+#     [4 ,5],
+#     [3 ,8]]
+
+# result = [[0,0,0],
+#          [0,0,0]]
+
+
+# for i in range(len(X)):
+#     for j in range(len(X[0])):
+#         result[j][i] = X[i][j]
+
+# for r in result:
+#     print(r)
+
+
+# method 2
+# Program to transpose a matrix using list comprehension
 
 X = [[12,7],
     [4 ,5],
     [3 ,8]]
 
-result = [[0,0,0],
-         [0,0,0]]
+result = [[X[j][i] for j in range(len(X))] for i in range(len(X[0]))]
 
-
-for i in range(len(X)):
-    for j in range(len(X[0])):
-        result[j][i] = X[i][j]
-
-for r in result:
-    print(r)
+print(result)
