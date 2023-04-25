debedce764ba0a9cad931e00fc017782ecbb5243
Jijo James <jj.jamesjijo@gmail.com>
2023-04-25 23:57:19 +0530

    practice/ set operations

---
+++ practice/set_operations.py
@@ -0,0 +1,12 @@
+# Program to perform different set operations like in mathematics
+
+E = {0, 2, 4, 6, 8}
+N = {1, 2, 3, 4, 5}
+
+print("Union of E and N is: ",E | N)
+
+print("Intersection of E and N is: ",E & N)
+
+print("Difference of E and N is: ",E - N)
+
+print("Symmetric difference of E and N is: ",E ^ N)
