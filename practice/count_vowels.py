cfb5d4ebb9b9d5f60a87918f1746d097a44acb1e
Jijo James <jj.jamesjijo@gmail.com>
2023-04-26 00:05:37 +0530

    practice/ merge mails from name and body from different text files

--- practice/count_vowels.py
+++ practice/count_vowels.py
@@ -7,7 +7,7 @@ ip_str = "Hello, have you tried our tutorial section yet?"
 ip_str = ip_str.casefold()
 
 count = {}.fromkeys(vowels, 0)
-print(count)
+
 for char in ip_str:
     if char in count:
         count[char] += 1
