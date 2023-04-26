e317453f7ef21b7a21313511fb69440d9e84bd08
Jijo James <jj.jamesjijo@gmail.com>
2023-04-26 23:46:59 +0530

    practice/ half pyramid using numbers

--- practice/pyramid_patterns.py
+++ practice/pyramid_patterns.py
@@ -6,3 +6,13 @@ for i in range(rows):
     for j in range(i+1):
         print("* ", end="")
     print("\n")
+
+
+# Problem 1: Program to print half pyramid using numbers
+
+rows = int(input("Enter number of rows: "))
+
+for i in range(rows):
+    for j in range(i+1):
+        print(j+1, end=" ")
+    print("\n")
