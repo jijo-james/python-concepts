8b88048da5b2f41b2ccf4d885eed3b1c8a7a63e1
Jijo James <jj.jamesjijo@gmail.com>
2023-04-26 23:56:11 +0530

    practice/ inverted full pyramid

--- practice/pyramid_patterns.py
+++ practice/pyramid_patterns.py
@@ -73,27 +73,41 @@
 #     print()
 
 
-# Problem 7: Program to print pyramid using numbers
+# # Problem 7: Program to print pyramid using numbers
+
+# rows = int(input("Enter number of rows: "))
+
+# k = 0
+# count=0
+# count1=0
+
+# for i in range(1, rows+1):
+#     for space in range(1, (rows-i)+1):
+#         print("  ", end="")
+#         count+=1
+    
+#     while k!=((2*i)-1):
+#         if count<=rows-1:
+#             print(i+k, end=" ")
+#             count+=1
+#         else:
+#             count1+=1
+#             print(i+k-(2*count1), end=" ")
+#         k += 1
+    
+#     count1 = count = k = 0
+#     print()
+
+
+# Problem 8: Program to print inverted pyramid using *
 
 rows = int(input("Enter number of rows: "))
 
-k = 0
-count=0
-count1=0
-
-for i in range(1, rows+1):
-    for space in range(1, (rows-i)+1):
+for i in range(rows, 1, -1):
+    for space in range(0, rows-i):
         print("  ", end="")
-        count+=1
-    
-    while k!=((2*i)-1):
-        if count<=rows-1:
-            print(i+k, end=" ")
-            count+=1
-        else:
-            count1+=1
-            print(i+k-(2*count1), end=" ")
-        k += 1
-    
-    count1 = count = k = 0
+    for j in range(i, 2*i-1):
+        print("* ", end="")
+    for j in range(1, i-1):
+        print("* ", end="")
     print()
