35b8b2e91991dce44332e56ca029ce0d465a9ad3
Jijo James <jj.jamesjijo@gmail.com>
2023-04-26 23:48:26 +0530

    practice/ half pyramid using alphabets

--- practice/pyramid_patterns.py
+++ practice/pyramid_patterns.py
@@ -1,18 +1,33 @@
-# Problem 1: Program to print half pyramid using *
+# # Problem 1: Program to print half pyramid using *
+
+# rows = int(input("Enter number of rows: "))
+
+# for i in range(rows):
+#     for j in range(i+1):
+#         print("* ", end="")
+#     print("\n")
+
+
+# # Problem 1: Program to print half pyramid using numbers
+
+# rows = int(input("Enter number of rows: "))
+
+# for i in range(rows):
+#     for j in range(i+1):
+#         print(j+1, end=" ")
+#     print("\n")
+
+
+# Problem 1: Program to print half pyramid using alphabets
 
 rows = int(input("Enter number of rows: "))
 
-for i in range(rows):
-    for j in range(i+1):
-        print("* ", end="")
-    print("\n")
-
-
-# Problem 1: Program to print half pyramid using numbers
-
-rows = int(input("Enter number of rows: "))
+ascii_value = 65
 
 for i in range(rows):
-    for j in range(i+1):
-        print(j+1, end=" ")
+    for j in range(i + 1):
+        alphabet = chr(ascii_value)
+        print(alphabet, end=" ")
+
+    ascii_value += 1
     print("\n")
