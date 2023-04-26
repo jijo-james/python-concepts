f1591cc9a9d835fca478fbf985d94e632e7c152b
Jijo James <jj.jamesjijo@gmail.com>
2023-04-26 23:54:51 +0530

    practice/ full pyramid using numbers

--- practice/pyramid_patterns.py
+++ practice/pyramid_patterns.py
@@ -55,19 +55,45 @@
 #     print("\n")
 
 
-# Problem 6: Program to print pyramid using *
+# # Problem 6: Program to print pyramid using *
+
+# rows = int(input("Enter number of rows: "))
+
+# k = 0
+
+# for i in range(1, rows + 1):
+#     for space in range(1, (rows - i) + 1):
+#         print(end="  ")
+
+#     while k != (2 * i - 1):
+#         print("* ", end="")
+#         k += 1
+
+#     k = 0
+#     print()
+
+
+# Problem 7: Program to print pyramid using numbers
 
 rows = int(input("Enter number of rows: "))
 
 k = 0
+count=0
+count1=0
 
-for i in range(1, rows + 1):
-    for space in range(1, (rows - i) + 1):
-        print(end="  ")
-
-    while k != (2 * i - 1):
-        print("* ", end="")
+for i in range(1, rows+1):
+    for space in range(1, (rows-i)+1):
+        print("  ", end="")
+        count+=1
+    
+    while k!=((2*i)-1):
+        if count<=rows-1:
+            print(i+k, end=" ")
+            count+=1
+        else:
+            count1+=1
+            print(i+k-(2*count1), end=" ")
         k += 1
-
-    k = 0
+    
+    count1 = count = k = 0
     print()
