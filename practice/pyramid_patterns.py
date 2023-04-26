b9b825e8a5e443eb5eb78c4cc2cfeb97f99398b0
Jijo James <jj.jamesjijo@gmail.com>
2023-04-26 23:52:02 +0530

    practice/ inverted half pyramid using numbers

--- practice/pyramid_patterns.py
+++ practice/pyramid_patterns.py
@@ -33,12 +33,23 @@
 #     print("\n")
 
 
-# Problem : Program to print inverted half pyramid using *
+# # Problem 4: Program to print inverted half pyramid using *
+
+# rows = int(input("Enter number of rows: "))
+
+# for i in range(rows, 0, -1):
+#     for j in range(0, i):
+#         print("* ", end=" ")
+    
+#     print("\n")
+
+
+# Problem 5: Program to print inverted half pyramid using numbers
 
 rows = int(input("Enter number of rows: "))
 
 for i in range(rows, 0, -1):
-    for j in range(0, i):
-        print("* ", end=" ")
+    for j in range(1, i+1):
+        print(j, end=" ")
     
     print("\n")
