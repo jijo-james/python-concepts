077685483d94d87bb0ca388fc782782e8adc9376
Jijo James <jj.jamesjijo@gmail.com>
2023-04-27 17:53:17 +0530

    practice/ merge dictonaries using copy() and update()

--- practice/merge_dictionaries.py
+++ practice/merge_dictionaries.py
@@ -8,9 +8,20 @@
 # print(dict_1 | dict_2)
 
 
-# method 2: using * operator
+# # method 2: using ** operator
+
+# dict_1 = {1: 'a', 2: 'b'}
+# dict_2 = {2: 'c', 4: 'd'}
+
+# print({**dict_1, **dict_2})
+
+
+# method 3: using copy() and update()
 
 dict_1 = {1: 'a', 2: 'b'}
 dict_2 = {2: 'c', 4: 'd'}
 
-print({**dict_1, **dict_2})
+dict_3 = dict_2.copy()
+dict_3.update(dict_1)
+
+print(dict_3)
