e53c07495ae6bdc8d00e137ada02913769fb88da
Jijo James <jj.jamesjijo@gmail.com>
2023-04-27 18:04:15 +0530

    practice/ safely create nested directory - raise an exception if directory already exists

--- practice/nested_directories.py
+++ practice/nested_directories.py
@@ -14,8 +14,18 @@
 # os.makedirs("/root/dirA/dirB")
 
 
-# method 3:
+# # method 3:
 
-import distutils.dir_util
+# import distutils.dir_util
 
-distutils.dir_util.mkpath("/root/dirA/dirB")
+# distutils.dir_util.mkpath("/root/dirA/dirB")
+
+
+# method 4:
+
+import os
+
+try:
+    os.makedirs("/dirA/dirB")
+except FileExistsError:
+    print("File already exists")
