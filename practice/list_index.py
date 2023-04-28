16a74236d30dfd6012a6a7c959a7701bb44460d3
Jijo James <jj.jamesjijo@gmail.com>
2023-04-28 17:01:16 +0530

    practice/ finding list index using enumarate starting index as 1

--- practice/list_index.py
+++ practice/list_index.py
@@ -1,8 +1,15 @@
 # Python program to find index of list
 
-# method 1:
+# # method 1:
 
+# my_list = [21, 44, 35, 11]
+
+# for index, val in enumerate(my_list):
+#     print(index, val)
+
+
+# method 2:
 my_list = [21, 44, 35, 11]
 
-for index, val in enumerate(my_list):
+for index, val in enumerate(my_list, start=1):
     print(index, val)
