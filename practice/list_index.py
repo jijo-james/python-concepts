efe111bb7f05453ee8adc6feb96b0f4587325991
Jijo James <jj.jamesjijo@gmail.com>
2023-04-28 17:02:03 +0530

    practice/ finding list index without using enumarate

--- practice/list_index.py
+++ practice/list_index.py
@@ -8,8 +8,16 @@
 #     print(index, val)
 
 
-# method 2:
+# # method 2:
+# my_list = [21, 44, 35, 11]
+
+# for index, val in enumerate(my_list, start=1):
+#     print(index, val)
+
+
+# method 3:
 my_list = [21, 44, 35, 11]
 
-for index, val in enumerate(my_list, start=1):
-    print(index, val)
+for index in range(len(my_list)):
+    value = my_list[index]
+    print(index, value)
