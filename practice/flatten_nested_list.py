70c2a91a08a2109d7d8ab0331a975ab039d3079e
Jijo James <jj.jamesjijo@gmail.com>
2023-04-28 17:08:05 +0530

    practice/ flatten a nested list using itertools

--- practice/flatten_nested_list.py
+++ practice/flatten_nested_list.py
@@ -1,20 +1,30 @@
 # Python program for flatten a nested list
 
-# method 1:
+# # method 1:
+
+# my_list = [[1], [2, 3], [4, 5, 6, 7]]
+
+# flat_list = [num for sublist in my_list for num in sublist]
+# print(flat_list)
+
+
+# # method 2:
+
+# my_list = [[1], [2, 3], [4, 5, 6, 7]]
+
+# flat_list = []
+# for sublist in my_list:
+#     for num in sublist:
+#         flat_list.append(num)
+
+# print(flat_list)
+
+
+# method 3:
+
+import itertools
 
 my_list = [[1], [2, 3], [4, 5, 6, 7]]
 
-flat_list = [num for sublist in my_list for num in sublist]
-print(flat_list)
-
-
-# method 2:
-
-my_list = [[1], [2, 3], [4, 5, 6, 7]]
-
-flat_list = []
-for sublist in my_list:
-    for num in sublist:
-        flat_list.append(num)
-
+flat_list = list(itertools.chain(*my_list))
 print(flat_list)
