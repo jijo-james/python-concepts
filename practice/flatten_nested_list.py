338f9f9a92d52993273720223f5cd55684203b23
Jijo James <jj.jamesjijo@gmail.com>
2023-04-28 17:07:14 +0530

    practice/ flatten a nested list using nested for loop

--- practice/flatten_nested_list.py
+++ practice/flatten_nested_list.py
@@ -6,3 +6,15 @@ my_list = [[1], [2, 3], [4, 5, 6, 7]]
 
 flat_list = [num for sublist in my_list for num in sublist]
 print(flat_list)
+
+
+# method 2:
+
+my_list = [[1], [2, 3], [4, 5, 6, 7]]
+
+flat_list = []
+for sublist in my_list:
+    for num in sublist:
+        flat_list.append(num)
+
+print(flat_list)
