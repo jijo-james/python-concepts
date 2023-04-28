6eee85fec7c2242396992dc405781a290cb78c5b
Jijo James <jj.jamesjijo@gmail.com>
2023-04-28 17:10:34 +0530

    practice/ flatten a nested list using reduce and lambda

--- practice/flatten_nested_list.py
+++ practice/flatten_nested_list.py
@@ -30,9 +30,17 @@
 # print(flat_list)
 
 
-# method 4:
+# # method 4:
+
+# my_list = [[1], [2, 3], [4, 5, 6, 7]]
+
+# flat_list = sum(my_list, [])
+# print(flat_list)
+
+
+# method 5:
+
+from functools import reduce
 
 my_list = [[1], [2, 3], [4, 5, 6, 7]]
-
-flat_list = sum(my_list, [])
-print(flat_list)
+print(reduce(lambda x, y: x + y, my_list))
