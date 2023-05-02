5f2efc41b19829776f2cb10a9f386b5d9dde9d48
Jijo James <jj.jamesjijo@gmail.com>
2023-05-02 23:30:41 +0530

    practice/ list slicing - items from one position to another

--- practice/list_slicing.py
+++ practice/list_slicing.py
@@ -12,8 +12,14 @@
 
 # print(my_list[2:])
 
-# method 3
+# # method 3
+
+# my_list = [1, 2, 3, 4, 5]
+
+# print(my_list[:2])
+
+# method 4
 
 my_list = [1, 2, 3, 4, 5]
 
-print(my_list[:2])
+print(my_list[2:4])
