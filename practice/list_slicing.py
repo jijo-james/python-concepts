fce894d57b6b7ffe5f728e55ab7c7aacd81ec62c
Jijo James <jj.jamesjijo@gmail.com>
2023-05-02 23:29:35 +0530

    practice/ list slicing - items before a specific position

--- practice/list_slicing.py
+++ practice/list_slicing.py
@@ -6,8 +6,14 @@
 
 # print(my_list[:])
 
-# method 2
+# # method 2
+
+# my_list = [1, 2, 3, 4, 5]
+
+# print(my_list[2:])
+
+# method 3
 
 my_list = [1, 2, 3, 4, 5]
 
-print(my_list[2:])
+print(my_list[:2])
