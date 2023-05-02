0db73c036135d81a2ba0c1311e4212e3e49f8e71
Jijo James <jj.jamesjijo@gmail.com>
2023-05-02 23:28:54 +0530

    practice/ list slicing - items after a specific position

--- practice/list_slicing.py
+++ practice/list_slicing.py
@@ -1,7 +1,13 @@
 # Python program to slice lists
 
-# method 1
+# # method 1
+
+# my_list = [1, 2, 3, 4, 5]
+
+# print(my_list[:])
+
+# method 2
 
 my_list = [1, 2, 3, 4, 5]
 
-print(my_list[:])
+print(my_list[2:])
