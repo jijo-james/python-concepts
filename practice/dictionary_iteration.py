24d8478e58bb616fd60005df47a77a1dca63c62b
Jijo James <jj.jamesjijo@gmail.com>
2023-05-04 23:14:16 +0530

    practice/ dictionary iteration - without using items()

--- practice/dictionary_iteration.py
+++ practice/dictionary_iteration.py
@@ -1,7 +1,13 @@
 # Python program to iterate over dictonaries
 
-# method 1
+# # method 1
+# dt = {"a": "juice", "b": "grill", "c": "corn"}
+
+# for key, value in dt.items():
+#     print(key, value)
+
+# method 2
 dt = {"a": "juice", "b": "grill", "c": "corn"}
 
-for key, value in dt.items():
-    print(key, value)
+for key in dt:
+    print(key, dt[key])
