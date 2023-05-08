947a49ecdae7dd8ea755366d498f267e627602d0
Jijo James <jj.jamesjijo@gmail.com>
2023-05-08 23:51:05 +0530

    practice/ sort only the values

--- practice/sort_dictionary.py
+++ practice/sort_dictionary.py
@@ -1,7 +1,13 @@
 # Python program for sort dictionary by value
 
-# method 1
-dt = {5: 4, 1: 6, 6: 3}
+# # method 1
+# dt = {5: 4, 1: 6, 6: 3}
 
-sorted_dt = {key: value for key, value in sorted(dt.items(), key=lambda item: item[1])}
-print(sorted_dt)
+# sorted_dt = {key: value for key, value in sorted(dt.items(), key=lambda item: item[1])}
+# print(sorted_dt)
+
+# method 2
+
+dt = {5: 4, 1: 6, 6: 3}
+sorted_dt_values = sorted(dt.values())
+print(sorted_dt_values)
