9695433d97af93ded68ee222ed50072b29d4fb0e
Jijo James <jj.jamesjijo@gmail.com>
2023-05-08 23:58:42 +0530

    practice/ check empty list - using comparison

--- practice/check_empty_list.py
+++ practice/check_empty_list.py
@@ -4,11 +4,17 @@
 # my_list = []
 
 # if not my_list:
+# #     print("Oops... list is empty")
+
+# # method 2
+
+# my_list = []
+
+# if not len(my_list):
 #     print("Oops... list is empty")
 
-# method 2
+#method 3
 
 my_list = []
-
-if not len(my_list):
-    print("Oops... list is empty")
+if my_list == []:
+    print("The list is empty")
