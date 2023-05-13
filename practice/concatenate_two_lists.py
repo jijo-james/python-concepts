ac2fa66cadaaa8554d302040c0540714856fb481
Jijo James <jj.jamesjijo@gmail.com>
2023-05-13 23:54:08 +0530

    practice/ concatenate two lists - using unique values

--- practice/concatenate_two_lists.py
+++ practice/concatenate_two_lists.py
@@ -7,9 +7,16 @@
 # list_joined = list_1 + list_2
 # print(list_joined)
 
-# method 2
-list_1 = [1, 'a']
-list_2 = range(2, 4)
+# # method 2
+# list_1 = [1, 'a']
+# list_2 = range(2, 4)
 
-list_joined = [*list_1, *list_2]
+# list_joined = [*list_1, *list_2]
+# print(list_joined)
+
+# method 3
+list_1 = [1, 'a']
+list_2 = [1, 2, 3]
+
+list_joined = list(set(list_1 + list_2))
 print(list_joined)
