dd004f6d4855cd03aa9cf7d38793d83164fa74ee
Jijo James <jj.jamesjijo@gmail.com>
2023-05-14 23:45:12 +0530

    practice/ list to chunks - using numpy

--- practice/list_chunks.py
+++ practice/list_chunks.py
@@ -10,8 +10,14 @@
 # my_list = [1,2,3,4,5,6,7,8,9]
 # print(list(split(my_list, chunk_size)))
 
-# method 2
-chunk_size = 2
+# # method 2
+# chunk_size = 2
+# my_list = [1,2,3,4,5,6,7,8,9]
+# list_chunked = [my_list[i:i + chunk_size] for i in range(0, len(my_list), chunk_size)]
+# print(list_chunked)
+
+# method 3
+import numpy as np
+
 my_list = [1,2,3,4,5,6,7,8,9]
-list_chunked = [my_list[i:i + chunk_size] for i in range(0, len(my_list), chunk_size)]
-print(list_chunked)
+print(np.array_split(my_list, 5))
