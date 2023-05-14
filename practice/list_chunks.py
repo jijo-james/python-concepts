395222a57c6f2bcc234bdcadf1d9ac6373e9f9c6
Jijo James <jj.jamesjijo@gmail.com>
2023-05-14 23:44:22 +0530

    practice/ list to chunks - using yield list comprehension

--- practice/list_chunks.py
+++ practice/list_chunks.py
@@ -1,10 +1,17 @@
 # Python program to split a list into evenly sized chunks
 
-def split(list_a, chunk_size):
+# # method 1
+# def split(list_a, chunk_size):
 
-  for i in range(0, len(list_a), chunk_size):
-    yield list_a[i:i + chunk_size]
+#   for i in range(0, len(list_a), chunk_size):
+#     yield list_a[i:i + chunk_size]
 
+# chunk_size = 2
+# my_list = [1,2,3,4,5,6,7,8,9]
+# print(list(split(my_list, chunk_size)))
+
+# method 2
 chunk_size = 2
 my_list = [1,2,3,4,5,6,7,8,9]
-print(list(split(my_list, chunk_size)))
+list_chunked = [my_list[i:i + chunk_size] for i in range(0, len(my_list), chunk_size)]
+print(list_chunked)
