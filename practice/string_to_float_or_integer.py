0dcf5dd65de0289dfeb7971a80fe555387c20175
Jijo James <jj.jamesjijo@gmail.com>
2023-05-14 23:50:49 +0530

    practice/ string to float

--- practice/string_to_float_or_integer.py
+++ practice/string_to_float_or_integer.py
@@ -1,9 +1,17 @@
 # Python program to parse a string to a float or int
 
-# method 1
-balance_str = "1500"
-balance_int = int(balance_str)
+# # method 1
+# balance_str = "1500"
+# balance_int = int(balance_str)
 
-print(type(balance_int))
+# print(type(balance_int))
 
-print(balance_int)
+# print(balance_int)
+
+# method 2
+balance_str = "1500.4"
+balance_float = float(balance_str)
+
+print(type(balance_float))
+
+print(balance_float)
