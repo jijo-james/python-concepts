76ffde8c8288f53cc61697886894ea7a6ce0d0c9
Jijo James <jj.jamesjijo@gmail.com>
2023-05-15 22:28:34 +0530

    practice/ colored text to terminal - using python module termcolor

--- practice/colored_text.py
+++ practice/colored_text.py
@@ -1,5 +1,12 @@
 # Python program to print colored text to the terminal
 
-# method 1
+# # method 1
 
-print("\x1b[38;2;5;86;243m" + "Programiz" + "\x1b[0m")
+# print("\x1b[38;2;5;86;243m" + "Programiz" + "\x1b[0m")
+
+
+#method 2
+
+from termcolor import colored
+
+print(colored('Programiz', 'blue'))
