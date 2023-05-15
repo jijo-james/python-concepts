8aa4d03c52c2ae95f8c49a3c953641e8297f62b5
Jijo James <jj.jamesjijo@gmail.com>
2023-05-15 22:32:28 +0530

    practice/ string to datetime - using datetime module

---
+++ practice/string_to_datetime.py
@@ -0,0 +1,13 @@
+# Python program to convert string to datetime
+
+# method 1
+
+from datetime import datetime
+
+my_date_string = "Mar 11 2011 11:31AM"
+
+datetime_object = datetime.strptime(my_date_string, "%b %d %Y %I:%M%p")
+
+print(type(datetime_object))
+print(datetime_object)
+
