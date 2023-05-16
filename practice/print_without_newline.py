3780cb88ab0fc9918fe0f13c8cd9d049ff83531f
Jijo James <jj.jamesjijo@gmail.com>
2023-05-16 23:39:22 +0530

    practice/ print output without a newline

---
+++ practice/print_without_newline.py
@@ -0,0 +1,13 @@
+# Python program to print without a newline
+
+
+# print each statement on a new line
+print("Python")
+print("is easy to learn.")
+
+# new line
+print()
+
+# print both the statements on a single line
+print("Python", end=" ")
+print("is easy to learn.")
