9199a3d0ad082c574f66bb7459b8b8f1beb11625
Jijo James <jj.jamesjijo@gmail.com>
2023-05-16 23:36:43 +0530

    practice/ string slicing

---
+++ practice/string_slicing.py
@@ -0,0 +1,12 @@
+# Python program to print the substring of a string
+
+my_string = "I love python."
+
+# prints "love"
+print(my_string[2:6])
+
+# prints "love python."
+print(my_string[2:])
+
+# prints "I love python"
+print(my_string[:-1])
