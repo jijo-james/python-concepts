5c4c88534a81fee3b444666ac696f2b24bf18f55
Jijo James <jj.jamesjijo@gmail.com>
2023-05-18 23:29:57 +0530

    practice/ read a file line by line into a list using list comprehension

--- practice/read_linebyline.py
+++ practice/read_linebyline.py
@@ -1,14 +1,25 @@
 # Python program to read a file line by line into a list
 
 
-# method 1
-with open("data_file.txt") as f:
-    content_list = f.readlines()
+# # method 1
+# with open("data_file.txt") as f:
+#     content_list = f.readlines()
+
+# print(content_list)
+
+# # remove new line characters
+# content_list = [x.strip() for x in content_list]
+# print(content_list)
+
+
+# method 2
+with open('data_file.txt') as f:
+    content_list = [line for line in f]
 
 print(content_list)
 
-# remove new line characters
-content_list = [x.strip() for x in content_list]
+# removing the characters
+with open('data_file.txt') as f:
+    content_list = [line.rstrip() for line in f]
+
 print(content_list)
-
-
