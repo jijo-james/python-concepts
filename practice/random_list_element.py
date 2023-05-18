a1bd67ebb320baf0821b04843fc2323f7efb9374
Jijo James <jj.jamesjijo@gmail.com>
2023-05-18 23:33:39 +0530

    practice/ random list element - using secrets module

--- practice/random_list_element.py
+++ practice/random_list_element.py
@@ -1,8 +1,15 @@
 # Python program to randomly select an element from the list
 
 
-# method 1
-import random
+# # method 1
+# import random
+
+# my_list = [1, 'a', 32, 'c', 'd', 31]
+# print(random.choice(my_list))
+
+
+# method 2
+import secrets
 
 my_list = [1, 'a', 32, 'c', 'd', 31]
-print(random.choice(my_list))
+print(secrets.choice(my_list))
