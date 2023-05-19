70d437ace789bbeda72723608c5ff9fa1ca19626
Jijo James <jj.jamesjijo@gmail.com>
2023-05-19 23:25:19 +0530

    practice/ check if a string is number - using int()

--- practice/check_string_number.py
+++ practice/check_string_number.py
@@ -1,13 +1,25 @@
 # Python program to check if a string is a number
 
 
-# method 1
-def isfloat(num):
+# # method 1
+# def isfloat(num):
+#     try:
+#         float(num)
+#         return True
+#     except ValueError:
+#         return False
+
+# print(isfloat('s12'))
+# print(isfloat('1.123'))
+
+
+# method 2
+def isint(num):
     try:
-        float(num)
+        int(num)
         return True
     except ValueError:
         return False
 
-print(isfloat('s12'))
-print(isfloat('1.123'))
+print(isint('s12'))
+print(isint('1123'))
