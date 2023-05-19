998fff90708c003796c168e0bf2157e540c20e5e
Jijo James <jj.jamesjijo@gmail.com>
2023-05-19 23:36:41 +0530

    practice/ delete from dictionary - using pop()

--- practice/delete_from_dictionary.py
+++ practice/delete_from_dictionary.py
@@ -1,9 +1,17 @@
 # Python program to delete an element from a dictionary
 
 
-# method 1
+# # method 1
+# my_dict = {31: "a", 21: "b", 14: "c"}
+
+# del my_dict[31]
+
+# print(my_dict)
+
+
+# method 2
 my_dict = {31: "a", 21: "b", 14: "c"}
 
-del my_dict[31]
+print(my_dict.pop(31))
 
 print(my_dict)
