cd8aaf4f9fb935da2c70141b3dfc69be9919fdfe
Jijo James <jj.jamesjijo@gmail.com>
2023-05-22 23:44:32 +0530

    practice/ extract file extension - using pathlib

--- practice/extract_file_extension.py
+++ practice/extract_file_extension.py
@@ -1,8 +1,13 @@
 # Python program to extract file extension
 
 
-# method 1
-import os
-file_details = os.path.splitext('/jijo.jpeg')
-print(file_details)
-print(file_details[1])
+# # method 1
+# import os
+# file_details = os.path.splitext('/jijo.jpeg')
+# print(file_details)
+# print(file_details[1])
+
+
+# method 2
+import pathlib
+print(pathlib.Path('/path/file.ext').suffix)
