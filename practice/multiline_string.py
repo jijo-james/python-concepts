81a6e275795de81a32043615fb3912ae251a2588
Jijo James <jj.jamesjijo@gmail.com>
2023-05-22 23:39:32 +0530

    practice/ multiline string - using "

--- practice/multiline_string.py
+++ practice/multiline_string.py
@@ -9,9 +9,17 @@
 # print(my_string)
 
 
-# method 2
-my_string = ("The only way to \n"
-        	"learn to program is \n"
-        	"by writing code.")
+# # method 2
+# my_string = ("The only way to \n"
+#         	"learn to program is \n"
+#         	"by writing code.")
+
+# print(my_string)
+
+
+# method 3
+my_string = "The only way to \n" \
+        	"learn to program is \n" \
+        	"by writing code."
 
 print(my_string)
