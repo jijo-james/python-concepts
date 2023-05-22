d84da4ee376c05f900dcfe3a81b0a17d493543ca
Jijo James <jj.jamesjijo@gmail.com>
2023-05-22 23:38:23 +0530

    practice/ multiline string - using parantheses and quotes

--- practice/multiline_string.py
+++ practice/multiline_string.py
@@ -1,9 +1,17 @@
 # Python program to print long multiline string
 
 
-# method 1
-my_string = '''The only way to
-learn to program is
-by writing code.'''
+# # method 1
+# my_string = '''The only way to
+# learn to program is
+# by writing code.'''
+
+# print(my_string)
+
+
+# method 2
+my_string = ("The only way to \n"
+        	"learn to program is \n"
+        	"by writing code.")
 
 print(my_string)
