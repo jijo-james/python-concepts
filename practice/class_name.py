1203237ed3eb032ea7c5573d0d9cb78b742b0f01
Jijo James <jj.jamesjijo@gmail.com>
2023-05-23 23:58:13 +0530

    practice/ class name - using type() and __name__

--- practice/class_name.py
+++ practice/class_name.py
@@ -1,10 +1,19 @@
 # Python program to get the class name of an instance
 
 
-# method 1
+# # method 1
+# class Vehicle:
+#     def name(self, name):
+#         return name
+
+# v = Vehicle()
+# print(v.__class__.__name__)
+
+
+# method 2
 class Vehicle:
     def name(self, name):
         return name
 
 v = Vehicle()
-print(v.__class__.__name__)
+print(type(v).__name__)
