3fc582cf06dc9b9271a3b94555969a0c13b446f4
Jijo James <jj.jamesjijo@gmail.com>
2023-05-23 23:54:56 +0530

    practice/ elapsed time - using timeit()

--- practice/elapsed_time.py
+++ practice/elapsed_time.py
@@ -1,12 +1,23 @@
 # Python program to measure elapsed time in python
 
 
-# method 1
-import time
+# # method 1
+# import time
 
-start = time.time()
+# start = time.time()
+
+# print(23*2.3)
+
+# end = time.time()
+# print(end - start)
+
+
+# method 2
+from timeit import default_timer as timer
+
+start = timer()
 
 print(23*2.3)
 
-end = time.time()
+end = timer()
 print(end - start)
