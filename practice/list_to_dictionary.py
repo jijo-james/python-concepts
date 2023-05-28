7f4fb7c6d9e4b3df4712fad448484acb63745483
Jijo James <jj.jamesjijo@gmail.com>
2023-05-28 23:28:24 +0530

    practice/ list to dictionary - using list comprehension

--- practice/list_to_dictionary.py
+++ practice/list_to_dictionary.py
@@ -1,9 +1,17 @@
 # Python program to convert two lists into a dictionary
 
 
-# method 1
+# # method 1
+# index = [1, 2, 3]
+# languages = ['python', 'c', 'c++']
+
+# dictionary = dict(zip(index, languages))
+# print(dictionary)
+
+
+# method 2
 index = [1, 2, 3]
 languages = ['python', 'c', 'c++']
 
-dictionary = dict(zip(index, languages))
+dictionary = {k: v for k, v in zip(index, languages)}
 print(dictionary)
