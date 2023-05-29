bb7ead63880351b432c96c58efe684483a915abb
Jijo James <jj.jamesjijo@gmail.com>
2023-05-29 23:36:05 +0530

    practice/ trim whitespace - using regular expression

--- practice/trim_whitespace.py
+++ practice/trim_whitespace.py
@@ -1,7 +1,16 @@
 # Python program to trim whitespace from a string
 
 
-# method 1
-my_string = " Python "
+# # method 1
+# my_string = " Python "
 
-print(my_string.strip())
+# print(my_string.strip())
+
+
+# method 2
+import re
+
+my_string  = " Hello Python "
+output = re.sub(r'^\s+|\s+$', '', my_string)
+
+print(output)
