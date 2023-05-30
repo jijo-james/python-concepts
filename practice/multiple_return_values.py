ae96201a3abce07edb5e8d44e2c59d87e578d7e6
Jijo James <jj.jamesjijo@gmail.com>
2023-05-30 23:57:08 +0530

    practice/ multiple return values - using dictionary

--- practice/multiple_return_values.py
+++ practice/multiple_return_values.py
@@ -1,12 +1,20 @@
 # Python program to return multiple values from a function
 
 
-# method1
+# # method1
+# def name():
+#     return "Jijo", "James"
+
+
+# print(name())
+
+# name_1, name_2 = name()
+# print("first name: ", name_1, "\n", "second name: ", name_2)
+
+
+# method 2
 def name():
-    return "Jijo", "James"
+    return {1: "Jijo", 2: "James"}
 
 
 print(name())
-
-name_1, name_2 = name()
-print("first name: ", name_1, "\n", "second name: ", name_2)
