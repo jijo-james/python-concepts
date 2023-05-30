49867ebc8be5293b0d6f1cb23b6cce22a1fff75c
Jijo James <jj.jamesjijo@gmail.com>
2023-05-31 00:04:27 +0530

    practice/ line count - using list comprehension

--- practice/line_count.py
+++ practice/line_count.py
@@ -1,12 +1,18 @@
 # Python program to print line count of a file
 
 
-# method 1
-def file_legth(filename):
-    with open(filename) as f:
-        for i, l in enumerate(f):
-            pass
-    return i + 1
+# # method 1
+# def file_legth(filename):
+#     with open(filename) as f:
+#         for i, l in enumerate(f):
+#             pass
+#     return i + 1
 
 
-print(file_legth("file.txt"))
+# print(file_legth("file.txt"))
+
+
+# method 2
+number_of_line = sum(1 for line in open("text.txt"))
+
+print(number_of_line)
