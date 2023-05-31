471b7c68e3f05635d1ef0dc9e390e5289471ee4d
Jijo James <jj.jamesjijo@gmail.com>
2023-06-01 00:40:34 +0530

    practice/ file modification time - using stat()

--- practice/file_modification_date.py
+++ practice/file_modification_date.py
@@ -1,10 +1,20 @@
 # Python program to to get a file creation and modification date
 
 
-# method 1
-import os.path, time, pathlib
+# # method 1
+# import os.path, time, pathlib
 
-file = pathlib.Path("factorial.py")
+# file = pathlib.Path("factorial.py")
 
-print("Last modification time: %s" % time.ctime(os.path.getmtime(file)))
-print("Creation time: %s" % time.ctime(os.path.getctime(file)))
+# print("Last modification time: %s" % time.ctime(os.path.getmtime(file)))
+# print("Creation time: %s" % time.ctime(os.path.getctime(file)))
+
+
+# method 2
+from datetime import datetime
+import pathlib
+
+file_name = pathlib.Path("jijo.jpeg")
+
+print("Last modification time: %s" % datetime.fromtimestamp(file_name.stat().st_mtime))
+print("Creation time: %s" % datetime.fromtimestamp(file_name.stat().st_ctime))
