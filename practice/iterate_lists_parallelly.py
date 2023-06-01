a1ea7611c5e0642ab56aec5216f9fbb9ba04f575
Jijo James <jj.jamesjijo@gmail.com>
2023-06-01 23:45:57 +0530

    practice/ iterate lists parallelly - using itertools.zip_longest

--- practice/iterate_lists_parallelly.py
+++ practice/iterate_lists_parallelly.py
@@ -1,9 +1,19 @@
 # Python program to iterate through two lists in parallel
 
 
-# method 1
-list_1 = [1, 2, 3, 4]
-list_2 = ['a', 'b', 'c']
+# # method 1
+# list_1 = [1, 2, 3, 4]
+# list_2 = ['a', 'b', 'c']
 
-for i, j in zip(list_1, list_2):
+# for i, j in zip(list_1, list_2):
+#     print(i, j)
+
+
+# method 2
+from itertools import zip_longest
+
+list_1 = [1, 2, 3, 4]
+list_2 = ["a", "b", "c"]
+
+for i, j in zip_longest(list_1, list_2):
     print(i, j)
