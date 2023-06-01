e35c4a70b484e58f0469841a948f79a238af483a
Jijo James <jj.jamesjijo@gmail.com>
2023-06-01 23:36:56 +0530

    practice/ cwd - using os

--- practice/current_working_directory.py
+++ practice/current_working_directory.py
@@ -1,8 +1,15 @@
 # Python program to get the full path of the current working directory
 
 
-# method 1
-import pathlib
+# # method 1
+# import pathlib
 
-print(pathlib.Path('jijo.jpeg').parent.absolute())
-print(pathlib.Path().absolute())
+# print(pathlib.Path("jijo.jpeg").parent.absolute())
+# print(pathlib.Path().absolute())
+
+
+# method 2
+from os import path, getcwd
+
+print(path.dirname(path.abspath("jijo.jpeg")))
+print(path.abspath(getcwd()))
