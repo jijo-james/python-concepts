a8630b9b1faeb4dce7fbff631a03a2377220bac2
Jijo James <jj.jamesjijo@gmail.com>
2023-06-02 14:14:36 +0530

    practice/ check filesize - using pathlib

--- practice/check_filesize.py
+++ practice/check_filesize.py
@@ -1,8 +1,15 @@
 # Python program to check the filesize
 
 
-# method 1
-from os import stat
+# # method 1
+# from os import stat
 
-file_size = stat("jijo.jpeg").st_size
-print(file_size)
+# file_size = stat("jijo.jpeg").st_size
+# print(file_size)
+
+
+# method 2
+from pathlib import Path
+
+file = Path("jijo.jpeg")
+print(file.stat().st_size)
