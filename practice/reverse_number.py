d17b5ce13032b50e80fee9c690ceb0da3db96ce1
Jijo James <jj.jamesjijo@gmail.com>
2023-06-02 14:24:24 +0530

    practice/ reverse number - using string slicing

--- practice/reverse_number.py
+++ practice/reverse_number.py
@@ -1,13 +1,18 @@
 # Python program to reverse a number
 
 
-# method 1
-number = int(input("Enter a number: "))
-reversed_number = 0
+# # method 1
+# number = int(input("Enter a number: "))
+# reversed_number = 0
 
-while number != 0:
-    digit = number % 10
-    reversed_number = reversed_number * 10 + digit
-    number //= 10
+# while number != 0:
+#     digit = number % 10
+#     reversed_number = reversed_number * 10 + digit
+#     number //= 10
 
-print("Reversed number: ", reversed_number)
+# print("Reversed number: ", reversed_number)
+
+
+# method 2
+number = input("Enter a number: ")
+print(str(number[::-1]))
