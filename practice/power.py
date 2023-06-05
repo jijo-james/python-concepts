7b64b77e14758ff775fc7950c9a9d87ea75180a4
Jijo James <jj.jamesjijo@gmail.com>
2023-06-05 23:47:27 +0530

    practice/ power of a number - using pow()

--- practice/power.py
+++ practice/power.py
@@ -14,13 +14,22 @@
 # print(int(result))
 
 
-# method 2
+# # method 2
+# base = 3
+# exponent = 4
+
+# result = 1
+
+# for exponent in range(exponent, 0, -1):
+#     result *= base
+
+# print(int(result))
+
+
+# method 3
 base = 3
 exponent = 4
 
-result = 1
-
-for exponent in range(exponent, 0, -1):
-    result *= base
+result = pow(base, exponent)
 
 print(int(result))
