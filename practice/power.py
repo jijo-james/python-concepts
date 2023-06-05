ce38c07cfdafb62da482cf5e04f1ebd97365928b
Jijo James <jj.jamesjijo@gmail.com>
2023-06-05 23:45:09 +0530

    practice/ power of a number - using for loop

--- practice/power.py
+++ practice/power.py
@@ -1,14 +1,26 @@
 # Python program to calculate the power of a number
 
 
-# method 1
+# # method 1
+# base = 3
+# exponent = 4
+
+# result = 1
+
+# while exponent != 0:
+#     result *= base
+#     exponent -= 1
+
+# print(int(result))
+
+
+# method 2
 base = 3
 exponent = 4
 
 result = 1
 
-while exponent != 0:
+for exponent in range(exponent, 0, -1):
     result *= base
-    exponent -=1
 
 print(int(result))
