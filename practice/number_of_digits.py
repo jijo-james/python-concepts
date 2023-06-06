2cc79555a12ce42fd6c887bbad2a0732c151cb59
Jijo James <jj.jamesjijo@gmail.com>
2023-06-06 23:21:56 +0530

    practice/ number of digits - using len()

--- practice/number_of_digits.py
+++ practice/number_of_digits.py
@@ -1,11 +1,17 @@
 # Python program to count the number of digits present in a number
 
 
-# method 1
-number = 3424
-count = 0
+# # method 1
+# number = 3424
+# count = 0
 
-while number != 0:
-    number //= 10
-    count += 1
-print(count)
+# while number != 0:
+#     number //= 10
+#     count += 1
+# print(count)
+
+
+# method 2
+number = 34325
+
+print(len(str(number)))
