82714c0a4fc3d67178481a6c6e3046941342b128
Jijo James <jj.jamesjijo@gmail.com>
2023-06-08 00:32:14 +0530

    practice/ capitalize first character - using capitalize()

--- practice/capitalize_first_character.py
+++ practice/capitalize_first_character.py
@@ -1,6 +1,11 @@
 # Python program to capitalize first character of a string
 
 
-# method 1
-string = "my name is Jijo"
-print(string[0].upper()+string[1:])
+# # method 1
+# string = "my name is Jijo"
+# print(string[0].upper()+string[1:])
+
+
+# method 2
+string = "i am a software engineer"
+print(string.capitalize())
