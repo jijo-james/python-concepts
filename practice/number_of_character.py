19f57b46bff2c2b093ee0b21b149bb33b2675cfe
Jijo James <jj.jamesjijo@gmail.com>
2023-06-10 22:59:50 +0530

    practice/ number of character - using count()

--- practice/number_of_character.py
+++ practice/number_of_character.py
@@ -1,13 +1,20 @@
 # Python program to count the number of occurence of a character in a string
 
 
-# method 1
-count = 0
+# # method 1
+# count = 0
+# string = "jijo james"
+# character = "j"
+
+# for i in string:
+#     if i == character:
+#         count += 1
+
+# print(count)
+
+
+# method 2
 string = "jijo james"
-character = "j"
+character = "a"
 
-for i in string:
-    if i == character:
-        count += 1
-
-print(count)
+print(string.count(character))
