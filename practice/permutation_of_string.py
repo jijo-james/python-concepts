aa00d425f639eb267c421363fa2956b87c51ec00
Jijo James <jj.jamesjijo@gmail.com>
2023-06-10 22:39:06 +0530

    practice/ permutation of string - using itertools()

--- practice/permutation_of_string.py
+++ practice/permutation_of_string.py
@@ -1,16 +1,23 @@
 # Python program to create all the permutations of a string
 
 
-# method 1
-def get_permutations(string, i=0):
-    if i == len(string):
-        print("".join(string))
+# # method 1
+# def get_permutations(string, i=0):
+#     if i == len(string):
+#         print("".join(string))
 
-    for j in range(i, len(string)):
-        words = [c for c in string]
-        words[i], words[j] = words[j], words[i]
+#     for j in range(i, len(string)):
+#         words = [c for c in string]
+#         words[i], words[j] = words[j], words[i]
 
-        get_permutations(words, i + 1)
+#         get_permutations(words, i + 1)
 
 
-get_permutations("jio")
+# get_permutations("jio")
+
+
+# method 2
+from itertools import permutations
+
+words = ["".join(p) for p in permutations("jijo")]
+print(words)
