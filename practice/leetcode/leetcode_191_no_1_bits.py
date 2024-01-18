b6c73e5b96f8e4f227dbdc8570e9327a136b0490
Jijo James <jj.jamesjijo@gmail.com>
2024-01-19 00:05:28 +0530

    leetcode 191/ number of 1 bits - solution 3

--- practice/leetcode/leetcode_191_no_1_bits.py
+++ practice/leetcode/leetcode_191_no_1_bits.py
@@ -8,8 +8,20 @@ class Solution1:
             n = n >> 1
         return count 
 
+
 # Solution 2
 
-class Solution1:
+class Solution2:
     def hamming_weight(self, n: int) -> int:
         return bin(n).count('1')
+
+
+# Solution 3
+
+class Solution3:
+    def hamming_weight(self, n:int) -> int:
+        count = 0
+        while n:
+            n &= n-1
+            count += 1
+        return count 
