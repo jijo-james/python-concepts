9d420cc6ada11db9ded31c6c4b6a3a8076b58327
Jijo James <jj.jamesjijo@gmail.com>
2024-01-25 23:55:41 +0530

    leetcode 208/ reverse linked list - recursively

--- practice/leetcode/leetcode_206_reverse_linked_list.py
+++ practice/leetcode/leetcode_206_reverse_linked_list.py
@@ -19,3 +19,22 @@ class Solution1:
             current = nxt
         
         return previous
+    
+s1 = Solution1()
+head = [1,2,3,4,5]
+res1 = s1.reverseList(head)
+print(res1)
+
+class Solution2:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        if not head:
+            return None
+        
+        new_head = head
+        if head.next:
+            new_head = self.reverseList(head.next)
+            head.next.next = head
+        head.next = None
+
+        return new_head
